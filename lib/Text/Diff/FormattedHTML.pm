4bc99e261d55e10f4a28ea9e23e6b05a4c6fc3fa
ambs <ambs@cpan.org>
2011-07-28 15:22:11 +0100

    Remove -T from tests

--- lib/Text/Diff/FormattedHTML.pm
+++ lib/Text/Diff/FormattedHTML.pm
@@ -18,11 +18,11 @@ Text::Diff::FormattedHTML - Generate a colorful HTML diff of strings/files.
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
